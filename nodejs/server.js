fa0b7447ea4de7f2c65d542a3e8010d9ca0cc163
Thomas Wang <thomas.cywang.13@gmail.com>
2021-08-20 17:01:44 -0400

    Completed server and UI implementation. Need to verify data returned from Maryland API is correct

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,47 +1,143 @@
-'use strict';
+"use strict";
 
-const Hapi = require('@hapi/hapi');
+const Hapi = require("@hapi/hapi");
+const rp = require("request-promise");
+
+/**
+ * @descritipn fetch State of Maryland Payment Data from https://opendata.maryland.gov/resource/7syw-q4cy.json
+ * @returns Promise<{vendor_zip: string, total_amount: string}[]>
+ */
+async function fetchStateOfMarylandPaymentData(
+  { fiscalYear, numTopEntries } = { fiscalYear: 2015, numTopEntries: 3 }
+) {
+  function sanitizeZipcode(zipCodeStr) {
+    const LENGTH_OF_ZIP_CODE = 5;
+    if (zipCodeStr.length === LENGTH_OF_ZIP_CODE) {
+      return zipCodeStr;
+    }
+    const numberOfZerosToPrepend = LENGTH_OF_ZIP_CODE - zipCodeStr.length;
+    const zeroes = new Array(numberOfZerosToPrepend)
+      .fill(null)
+      .map((_) => "0")
+      .join("");
+    return `${zeroes}${zipCodeStr}`;
+  }
+  try {
+    const queryParams = {
+      $select: `vendor_zip, sum(amount) as total_amount`,
+      $where: `fiscal_year=${fiscalYear}`,
+      $group: `vendor_zip`,
+      $order: `total_amount`,
+    };
+    const fetchResult = await rp({
+      method: "GET",
+      uri: "https://opendata.maryland.gov/resource/7syw-q4cy.json",
+      qs: queryParams,
+      json: true,
+    });
+
+    const zipCodesWithTopSpending = fetchResult
+      .slice(-numTopEntries)
+      .map(({ vendor_zip, total_amount }) => ({
+        vendorZip: sanitizeZipcode(vendor_zip),
+        totalAmount: total_amount,
+      }));
+
+    return zipCodesWithTopSpending;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+}
+
+async function mapZipCodesToCoordinates(data) {
+  async function delay(ms = 1010) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+  async function fetchUnitedStatesPostalCodeCoordinatesAPI(postalCode) {
+    const apiUrl = "https://nominatim.openstreetmap.org/search";
+    return rp({
+      headers: {
+        "User-Agent": "Maryland App",
+      },
+      uri: apiUrl,
+      method: "GET",
+      qs: {
+        format: "json",
+        postalcode: postalCode,
+        country: "United States",
+      },
+      json: true,
+    });
+  }
+  const result = [];
+
+  for (const { vendorZip, totalAmount } of data) {
+    await delay();
+    const fetchCoordinatesResult = (
+      await fetchUnitedStatesPostalCodeCoordinatesAPI(vendorZip)
+    )[0];
+    result.push({
+      vendorZip,
+      totalAmount,
+      longitude: fetchCoordinatesResult.lon,
+      latitude: fetchCoordinatesResult.lat,
+    });
+  }
+  return result;
+}
 
 const server = new Hapi.Server({
-    port: 3000,
-    host: '0.0.0.0'
+  port: 3000,
+  host: "0.0.0.0",
 });
 
-// TODO load the data from external APIs and figure out the top zip codes by amounts
-// Expose that data for the UI
-// Maryland Data URL: https://opendata.maryland.gov/resource/3ycv-rxy9.json
-
-server.register([{ plugin: require('@hapi/inert') }]).then(() => {
+server
+  .register([{ plugin: require("@hapi/inert") }])
+  .then(fetchStateOfMarylandPaymentData)
+  .then(mapZipCodesToCoordinates)
+  .then((zipCodesWithTopSpending) => {
     server.route({
-        method: 'GET',
-        path: '/',
-        handler: function (_request, h) {
-            return h.file('ui/index.html');
-        }
+      method: "GET",
+      path: "/",
+      handler: function (_request, h) {
+        return h.file("ui/index.html");
+      },
+    });
+    server.route({
+      method: "GET",
+      path: "/map-data",
+      handler: function (_request) {
+        return zipCodesWithTopSpending;
+      },
     });
 
     server.route({
-        method: 'GET',
-        path: '/app.js',
-        handler: function (_request, h) {
-            return h.file('ui/app.js');
-        }
+      method: "GET",
+      path: "/app.js",
+      handler: function (_request, h) {
+        return h.file("ui/app.js");
+      },
     });
 
     server.route({
-        method: 'GET',
-        path: '/node_modules/{param*}',
-        handler: {
-            directory: {
-                path: 'node_modules',
-                listing: false,
-                index: true
-            }
-        }
+      method: "GET",
+      path: "/node_modules/{param*}",
+      handler: {
+        directory: {
+          path: "node_modules",
+          listing: false,
+          index: true,
+        },
+      },
     });
-
 
     return server.start();
-}).then(() => {
-    console.log('Server started');
-});
+  })
+  .then(() => {
+    console.log("Server started");
+  })
+  .catch(() => {
+    console.warn("An error occured.");
+    process.exit(-1);
+  });
--- nodejs/ui/app.js
+++ nodejs/ui/app.js
@@ -1,12 +1,29 @@
-'use strict';
+"use strict";
 
 (function () {
-    var app = angular.module('immuta.homework', ['ui-leaflet']);
+  var app = angular.module("immuta.homework", ["ui-leaflet"]);
 
-    app.controller('MapController', MapController);
-    MapController.$inject = ['$scope'];
-
-    function MapController($scope) {
-        // TODO Implement controller to load top data points
+  function MapController($scope, leafletData) {
+    function fetchMapData() {
+      return fetch("/map-data");
     }
-}());
+
+    leafletData.getMap().then(function (map) {
+      console.log({ map });
+      L.GeoIP.centerMapOnPosition(map, 15);
+    });
+
+    fetchMapData()
+      .then((result) => result.json())
+      .then((data) => {
+        data.forEach(({ longitude, latitude }) => {
+          leafletData.getMap("map-simple-map").then(function (map) {
+            L.marker(L.latLng(latitude, longitude)).addTo(map);
+          });
+        });
+      });
+  }
+
+  MapController.$inject = ["$scope", "leafletData"];
+  app.controller("MapController", MapController);
+})();
